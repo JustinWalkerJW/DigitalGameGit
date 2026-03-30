e7995aa8cc0e1aa058bac232a87a983829b206c7
JustinWalker <2964367986@qq.com>
2026-03-30 17:14:50 +1100

    airship controller critical bug met

---
+++ Assets/AirshipController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AirshipController : MonoBehaviour
+{
+    public static AirshipController instance;
+    public float buoyancy;
+    public float upperLimit;
+    public float criticalThreshold;
+    public float risingThreshold;
+    public float dropingThreshold;
+    public float balloonIntegrity;
+    public Vector3 speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
